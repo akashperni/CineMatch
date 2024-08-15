1cf81c4f5ae6d21bb0ac2eb111cd784dc70185f7
Perni.Akash <54469922+perniakash@users.noreply.github.com>
2024-08-15 13:43:26 -0400

    adding readme

---
+++ app.py
@@ -0,0 +1,80 @@
+import pickle
+import streamlit as st
+import requests
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}" if poster_path else ""
+    return full_path
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    
+    for i in distances[1:9]:  # Get the top 8 recommendations
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movie_names, recommended_movie_posters
+
+# Streamlit app
+st.header('🎥 CineMatch: Your Personalized Movie Recommender Engine 🍿')
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    
+    # First row of 4 movies
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    
+    # Second row of 4 movies
+    col5, col6, col7, col8 = st.columns(4)
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+    with col6:
+        st.text(recommended_movie_names[5])
+        st.image(recommended_movie_posters[5])
+    with col7:
+        st.text(recommended_movie_names[6])
+        st.image(recommended_movie_posters[6])
+    with col8:
+        st.text(recommended_movie_names[7])
+        st.image(recommended_movie_posters[7])
+
+
+st.warning(
+    "Note: The dataset is taken from kaggle (TMDB 5000 Movie Dataset) Link: https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata?select=tmdb_5000_movies.csv")
+hide_menu_style = """
+<style>
+#MainMenu {visibility: hidden;}
+</style>
+"""
+
+
+
